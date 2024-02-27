37d5ae550a9ce561b49d89a720a89b854c6768b4
Sijian Xia <39158528+xiasijian@users.noreply.github.com>
2024-02-27 15:07:15 +0800

    Delete atac_seq_pipeline_v1.0 directory

--- atac_seq_pipeline_v1.0/run_cluster.sh
+++
@@ -1,6 +0,0 @@
-
-module load anaconda
-source activate snakemake_env
-snakemake -np -s combined.py --unlock
-snakemake -c8 -s combined.py --cluster "sbatch -p cpuPartition -N 1 --mem=10000 --ntasks=1  --cpus-per-task=1 --job-name={rule} --output={rule}.%j.out --error={rule}.%j.err" --jobs 8 --rerun-incomplete
-
