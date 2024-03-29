3298b1970eac9de0e4d52c5871463763c7704007
Sijian Xia <39158528+xiasijian@users.noreply.github.com>
2024-03-29 10:19:37 +0800

    Add files via upload

--- ucsc_tss_process.R
+++ ucsc_tss_process.R
@@ -5,11 +5,11 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 library(dplyr)
 
-df = read.csv(file = "./mm9_ucsc_tss20240329.bed",sep = "\t")
+df = read.csv(file = "./ucsc_mm9_date20240329.bed",sep = "\t",check.names = F)
 
 transcripts <- df %>%
-  dplyr::select(X.name, chrom, txStart, txEnd, strand, name2) %>%
-  dplyr::rename(transcript = X.name, gene = name2)
+  dplyr::select(name, chrom, txStart, txEnd, strand, name2) %>%
+  dplyr::rename(transcript = name, gene = name2)
 
 table(duplicated(transcripts$transcript))
 # FALSE  TRUE 
@@ -41,3 +41,11 @@ tss_final <- tss_dup_filt1[order(tss_dup_filt1$chrom, tss_dup_filt1$tss_start, t
 
 write.table(tss_final[,c("chrom","tss_start","tss_end","strand","gene","transcript")],
             file = paste0("mm9_ucsc_tss20240329_clean.bed"),sep = "\t",col.names = F,row.names = F,quote = F)
+
+## export bed file for bedtools slop
+
+write.table(tss_final[,c("chrom","tss_end","tss_end","strand","gene","transcript")],
+            file = paste0("mm9_ucsc_tss20240329_clean_for_bedtools_slop.bed"),sep = "\t",col.names = F,row.names = F,quote = F)
+
+
+#bedtools slop -i mm9_ucsc_tss20240329_clean_for_bedtools_slop.bed -g ../chrom_size/mm9.chrom.sizes -b 1000 > mm9_ucsc_tss20240329_clean_extend1kb.bed
